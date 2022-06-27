2d1d3ff9bb157ab67eca4a8e056944549bb28f6e
whh1123 <58422140+whh1123@users.noreply.github.com>
2022-06-26 23:12:56 -0700

    changed return with div added

--- recipe/src/components/MenuList.js
+++ recipe/src/components/MenuList.js
@@ -5,7 +5,9 @@ import RecipiesCard  from './RecipeCard.js';
 const MenuList = () => {
     const recipes_list = useSelector(state => state.recipes_list.list);
     
-    return recipes_list.map((user) => (<RecipiesCard key={user.id} name={user.name} ingredients={user.ingredients} description={user.description}/>));
+    return <div> 
+        {recipes_list.map((user) => <RecipiesCard key={user.id} name={user.name} ingredients={user.ingredients} description={user.description}/>)} 
+        </div>
 }
 
 export default MenuList;
